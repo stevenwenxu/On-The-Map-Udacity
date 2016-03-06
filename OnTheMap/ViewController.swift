d64d4c86b6ce2dc02d7f7652e89bf52c9bf1b616
Steven Xu <steven.xu13@gmail.com>
2016-03-06 11:14:17 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Steven Xu on 2016-03-06.
+//  Copyright © 2016 Steven Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
