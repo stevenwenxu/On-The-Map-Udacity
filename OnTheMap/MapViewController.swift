6318f61f59f57bd5f2e6de4d77a00d650d9ff364
Steven Xu <steven.xu13@gmail.com>
2016-03-06 16:10:01 -0500

    Map view done

--- OnTheMap/MapViewController.swift
+++ OnTheMap/MapViewController.swift
@@ -7,13 +7,102 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
 	var appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+	let appID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+	let APIKEY = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+
+	let locationURL = NSURL(string: "https://api.parse.com/1/classes/StudentLocation?limit=100")!
+
+	@IBOutlet weak var mapView: MKMapView!
+	var annotations: [MKPointAnnotation] = []
+	var students: [StudentLocation] = []
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.getStudents()
+		self.mapView.delegate = self
+	}
+
+	// MARK: - events
 
 	@IBAction func logOutPressed(sender: UIBarButtonItem) {
 		self.appDelegate.sessionId = nil
 		self.dismissViewControllerAnimated(true, completion: nil)
+	}
+
+	// MARK: - 
+	func getStudents() {
+		let request = NSMutableURLRequest(URL: self.locationURL)
+		request.addValue(self.appID, forHTTPHeaderField: "X-Parse-Application-Id")
+		request.addValue(self.APIKEY, forHTTPHeaderField: "X-Parse-REST-API-Key")
+		let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+			if let error = error {
+				print(error.localizedDescription)
+			}
+			var json: AnyObject
+			do {
+				json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+			} catch {
+				print("Something happened")
+				return
+			}
+			if let results = json["results"] as? NSArray {
+				results.forEach {
+					let result = $0 as! NSDictionary
+					self.students.append(StudentLocation(
+						objectId: result["objectId"] as! String,
+						uniqueKey: result["uniqueKey"] as! String,
+						firstName: result["firstName"] as! String,
+						lastName: result["lastName"] as! String,
+						mapString: result["mapString"] as! String,
+						mediaUrl: result["mediaURL"] as! String,
+						latitude: result["latitude"] as! Double,
+						longitude: result["longitude"] as! Double))
+				}
+			}
+			dispatch_async(dispatch_get_main_queue()) {
+				self.drawAnnotations()
+			}
+
+		}
+		task.resume()
+	}
+
+	func drawAnnotations() {
+		for student in self.students {
+			let annotation = MKPointAnnotation()
+			annotation.coordinate = CLLocationCoordinate2D(latitude: student.latitude, longitude: student.longitude)
+			annotation.title = "\(student.firstName) \(student.lastName)"
+			annotation.subtitle = student.mediaUrl
+			self.annotations.append(annotation)
+		}
+		self.mapView.addAnnotations(self.annotations)
+	}
+
+	// MARK: - MKMapViewDelegate
+
+	func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+		let reuseId = "pin"
+		var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+		if pinView == nil {
+			pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+		}
+		pinView!.annotation = annotation
+		pinView!.canShowCallout = true
+		pinView!.pinColor = .Red
+		pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+		return pinView
+	}
+
+	func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+		if control == view.rightCalloutAccessoryView {
+			if let url = view.annotation?.subtitle! {
+				UIApplication.sharedApplication().openURL(NSURL(string: url)!)
+			}
+		}
 	}
 }
---
+++ OnTheMap/StudentLocation.swift
@@ -0,0 +1,20 @@
+//
+//  StudentLocation.swift
+//  OnTheMap
+//
+//  Created by Steven Xu on 2016-03-06.
+//  Copyright © 2016 Steven Xu. All rights reserved.
+//
+
+import Foundation
+
+struct StudentLocation {
+	let objectId: String
+	let uniqueKey: String
+	let firstName: String
+	let lastName: String
+	let mapString: String
+	let mediaUrl: String
+	let latitude: Double
+	let longitude: Double
+}
